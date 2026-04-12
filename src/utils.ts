eb038a9730ee0b12bc8d73f3fcb5395647463344
Sergei Chipiga <chipiga86@gmail.com>
2026-04-12 22:48:55 +0300

    kick event loop each N sync ops

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,3 @@
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+export const yieldEventLoop = () => new Promise(resolve => setImmediate(resolve));
 export const xnor = (a: boolean, b: boolean): boolean => a === b;
